241bae76959bcf7f0ce0d148fc609dae54a3f861
guilhermepferrari@gmail.com <guilhermepferrari@gmail.com>
2016-05-08 21:23:06 -0300

    Linked Lists skeleton and basic implementation[2]

--- src/test_linkedLists.c
+++ src/test_linkedLists.c
@@ -2,8 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
 /*
-test_linkedLists.c: this file will test linkedLists library. It will test each function.
+test_linkedLists.c: this file will test linkedLists library. It will test each function, except search and print which will be
+                    logically tested, since it will be used as part of some test functions.
 */
 
 list test_makeList()
@@ -13,9 +17,23 @@ list test_makeList()
 
 int test_deleteList(list targetList)
 {
-
+// this will delete a certain lst and check it does not exist anymore
+// output: TRUE or FALSE
 }
 
 int test_add()
 {
+// this function will add specific values in the list and search for its existence later on.
+// output: TRUE or FALSE
 }
+
+int test_remove()
+{
+// this function tests by removing the values previously assigned in test_add function and search for its existence later on.
+}
+
+int main (int argc, char *argv[])
+{
+// main function, where functions are called and results displayed.
+}
+
