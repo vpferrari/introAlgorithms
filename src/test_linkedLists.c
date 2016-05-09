a7ad016a2461844c925f4b8d54a21ef801d4a441
guilhermepferrari@gmail.com <guilhermepferrari@gmail.com>
2016-05-08 22:45:44 -0300

    Linked Lists skeleton and basic implementation[2]

--- src/test_linkedLists.c
+++ src/test_linkedLists.c
@@ -12,19 +12,65 @@ test_linkedLists.c: this file will test linkedLists library. It will test each f
 
 list test_makeList()
 {
-// this will create an arbitrary list. No automated tests expected, since makeList is user-input dependent. 
+// this will create an arbitrary list. No automated tests expected, since makeList is user-input dependent.
+// OUTPUT: the created list 
+
+    printf("[INFO]: starting test_makeList.\n");
+    printf("[INPUT]: insert the requested values as needed. Add a 0 (zero) to end the function call.\n");
+
+    list testList = makeList();
+    
+    if(testList != NULL){
+        printf("[SUCCESS]: test_makeList completed.\n");
+        return testList; 
+    } else {
+        printf("[ERROR]: makeList failed. Please check your code.\n");
+        return NULL;
+    }
 }
 
 int test_deleteList(list targetList)
 {
-// this will delete a certain lst and check it does not exist anymore
-// output: TRUE or FALSE
+    // this will delete a certain lst and check it does not exist anymore
+    // OUTPUT: TRUE or FALSE
+    printf("[INFO]: starting test_deleteList.\n");
+    deleteList(targetList);
+
+    if(targetList == NULL) {
+        printf("[SUCCESS]: test_deleteList completed.\n");
+        return TRUE;
+    } else {
+        printf("[ERROR]: deleteList failed. Please check your code.\n");
+        return FALSE;
+    }
 }
 
-int test_add()
+int test_add(list testList)
 {
-// this function will add specific values in the list and search for its existence later on.
-// output: TRUE or FALSE
+    // this function will add specific values in the list and search for its existence later on.
+    // output: TRUE or FALSE
+    int testValues[] = {11, 42, 111, 666};
+    int testLength = sizeof(testValues)/sizeof(int);
+
+    for(i = 0; i < testLength; i++){
+        add(testValues[i], testList);
+    }
+
+    int count = 0; // a variable will be initially false
+
+    for(i = 0; i < testLength; i++){
+        if(search(testValues[i]) == TRUE){
+            count++;
+        }
+    }
+
+    if(count == testLength){
+        printf("[SUCCESS]: test_add completed.\n");
+        return TRUE;
+    } else {
+        printf("[ERROR]: add function failed. Please check your code.\n");
+        return FALSE;
+    }
 }
 
 int test_remove()
